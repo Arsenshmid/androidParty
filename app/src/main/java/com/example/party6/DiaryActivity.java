ba2cec234077981fe8f0755b1555513ed8237ff8
Arsenshmid <apce2@mail.ru>
2024-04-24 20:33:42 +0900

    autocommit

--- app/src/main/java/com/example/party6/DiaryActivity.java
+++ app/src/main/java/com/example/party6/DiaryActivity.java
@@ -1,4 +1,19 @@
 package com.example.party6;
 
-public class DiaryActivity {
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DiaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary);
+
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("Дневник"); // Пример установки текста в TextView
+    }
 }
